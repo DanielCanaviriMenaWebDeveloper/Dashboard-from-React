7abe16e31714619ca273ba8512f24308ab4c0fb5
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-07-08 22:51:16 -0400

    Creación de Componentes Navbar, Sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from "../src/components/Navbar";
+import Sidebar from "../src/components/Sidebar";
+
+import Clients from '../src/pages/Clients'
+import Home from '../src/pages/Home'
+import Sales from '../src/pages/Sales'
+	
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Navbar />
+
+			<div className="flex">
+				<Sidebar />
+
+				<div className="content">
+					<Route path="/" exact={true} component={Home} />
+					<Route path="/sales" exact={true} component={Sales} />
+					<Route path="/clients" exact={true} component={Clients} />
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
